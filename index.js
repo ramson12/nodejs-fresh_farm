3691591242394c48fe1c239ff0cfe3ac56a30052
ramson12 <48708494+ramson12@users.noreply.github.com>
2019-09-03 20:58:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,6 @@ const http = require('http');
 const url = require('url');
 
 const replaceTemplate = require('./modules/replacetemplate');
-///////////////////////////////////////
-// FILES
-
-// Blocking - synchronous way 
-// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-// console.log(textIn);
-// const textOut = `This is what we know about the avacado: ${textIn}.\n Created on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt',textOut);
-// console.log('file writen !');
-
-
-//Non-blocking , asynchronous way
-// fs.readFile('./txt/start.txt', 'utf-8' ,(err,data1) =>{
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err,data2) =>{
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt', 'utf-8' ,(err,data3) =>{
-//             console.log(data3);
-//             fs.writeFile('./txt/final.txt',`${data2} \n ${data3}`,'utf-8',err => {
-//                 console.log("Yoour file has been written");
-//             });
-//         });
-//     });
-// });
-// console.log("will read file");
 
 
 ////////////////////////////////////
@@ -38,7 +14,7 @@ const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.h
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8'); // read data from a json file
 const dataObj = JSON.parse(data); 
 
 
