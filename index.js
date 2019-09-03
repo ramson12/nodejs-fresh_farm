72d4237ae25820e8de6479b6bbc7f6a8671bbbd8
ramson12 <48708494+ramson12@users.noreply.github.com>
2019-09-03 20:18:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
+
+////////////////////////////////////
+// SERVER 
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image); 
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUNTITY%}/g,product.quantity); 
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');      
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data); 
+
+
+
+
+
+const server = http.createServer( (req, res) => {
+
+    const {query, pathname} = url.parse(req.url,true);
+
+    //overview page
+    if(pathname == '/' || pathname == '/overview'){
+        //call the overview template
+        res.writeHead(200,{'content-type' : 'text/html'});
+
+
+        // loop to the data object which holds all info adn with each eitherations we replace the cards which is el
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); //join to all d elements to a sting
+        
+        //console.log(cardsHtml); cardsHtml will returl all d cards as per d data in d array
+        // rempace the cards
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+        res.end(output);
+
+        //product page
+    } else if(pathname == '/product'){
+        res.writeHead(200,{'content-type' : 'text/html'});
+        const product = dataObj[query.id]; // get id from the url of a product
+        const output = replaceTemplate(tempProduct, product); 
+        res.end(output);
+
+        //api
+    }else if (pathname == '/api'){
+        res.writeHead(200,{'content-type' : 'application/json'});
+        res.end(data);
+
+        //not found
+    }else{
+        res.writeHead(404,{
+            'content-type':'text/html',
+            'my-own-header' : 'hello world'
+        });
+        res.end('<h1> PAGE NOT  FOUND </h1>')
+    }
+});
+
+server.listen(8000, '127.0.0.1',() => {
+    console.log("Listening to request on port 8000");
+});
+
+
