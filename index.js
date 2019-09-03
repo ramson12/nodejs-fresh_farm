84832c9a5d48dc2c4a4212c1094758e0ef504620
ramson12 <48708494+ramson12@users.noreply.github.com>
2019-09-03 20:45:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replacetemplate');
+///////////////////////////////////////
+// FILES
+
+// Blocking - synchronous way 
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avacado: ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('file writen !');
+
+
+//Non-blocking , asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8' ,(err,data1) =>{
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err,data2) =>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8' ,(err,data3) =>{
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt',`${data2} \n ${data3}`,'utf-8',err => {
+//                 console.log("Yoour file has been written");
+//             });
+//         });
+//     });
+// });
+// console.log("will read file");
 
 
 ////////////////////////////////////
 // SERVER 
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-    output = output.replace(/{%IMAGE%}/g,product.image); 
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%FROM%}/g,product.from);
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replace(/{%QUNTITY%}/g,product.quantity); 
-    output = output.replace(/{%DESCRIPTION%}/g,product.description);
-    output = output.replace(/{%ID%}/g,product.id);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');      
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
